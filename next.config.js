682c64baa45a1309770e606074d61381dc87ce86
John Kane <80558805+John668582@users.noreply.github.com>
2023-02-22 11:04:51 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["medusa-server-img.s3.us-west-2.amazonaws.com"],
   },
 })
 
