a5d99e57fa21347cf6d6fe3fb26c0c181b8f5d94
John668582 <1065659094@qq.com>
2023-02-23 11:08:01 +0800

    add the api key

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -6,13 +6,13 @@ const Hero = () => {
     <div className="h-[90vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
         <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Summer styles are finally here hahaha change succeeded
+          See the world in a new light with our stylish eyewear collection.
         </h1>
         <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-          This year, our new summer collection will shelter you from the harsh
-          elements of a world that doesn&apos;t care if you live or die.
+          
+          Choose from a wide range of premium frames and lenses, and discover the perfect pair to suit your unique style and needs
         </p>
-        <UnderlineLink href="/store">Explore products</UnderlineLink>
+        <UnderlineLink href="/store">Discover New Glasses</UnderlineLink>
       </div>
       <Image
         src="/hero.jpg"
